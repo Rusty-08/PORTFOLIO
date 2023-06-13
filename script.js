e76caeced916d84c77f0dad5126ab43729716140
unknown <gunaorusty@gmail.com>
2023-06-13 12:11:11 +0800

    add contact section

--- script.js
+++ script.js
@@ -1,22 +1,53 @@
-document.querySelectorAll('.navbar-nav .nav-link').forEach(function(link) {
-    link.addEventListener('click', function(e) {
-        e.preventDefault();
+function smoothScroll(target, duration) {
+  const targetElement = document.querySelector(target);
+  const navbarHeight = document.querySelector('.navbar').offsetHeight;
+  const targetPosition = targetElement.getBoundingClientRect().top + window.scrollY - navbarHeight;
+  const startPosition = window.scrollY;
+  const distance = targetPosition - startPosition;
+  let startTime = null;
 
-    var target = document.querySelector(this.getAttribute('href'));
-    var navbarHeight = document.querySelector('.navbar').offsetHeight;
-    var offset = target.offsetTop - navbarHeight;
+  function animation(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    const timeElapsed = currentTime - startTime;
+    const ease = easeInOutCubic(timeElapsed, startPosition, distance, duration);
+    window.scrollTo(0, ease);
+    if (timeElapsed < duration) requestAnimationFrame(animation);
+  }
 
-    window.scrollTo({
-        top: offset,
-        behavior: 'smooth'
-    });
-    });
+  function easeInOutCubic(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t * t + b;
+    t -= 2;
+    return (c / 2) * (t * t * t + 2) + b;
+  }
+
+  requestAnimationFrame(animation);
+}
+
+// Smooth scroll when  nav-link is clicked
+const navLinks = document.querySelectorAll('.nav-link');
+const contactLink = document.querySelector('.contact');
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    const target = link.getAttribute('href');
+    const duration = 400;
+    smoothScroll(target, duration);
+  });
 });
 
+// Hide navbar toggler when clicked 
 document.querySelectorAll('.navbar-nav .nav-link').forEach(function(link) {
-    link.addEventListener('click', function() {
-      if (window.innerWidth < 992) {
-        document.querySelector('.navbar-collapse').classList.remove('show');
-      }
-    });
+  link.addEventListener('click', function() {
+    if (window.innerWidth < 992) {
+      document.querySelector('.navbar-collapse').classList.remove('show');
+    }
   });
+});
+
+// Get the current year
+
+const currentYear = new Date().getFullYear();
+  
+  document.getElementById("current-year").textContent = currentYear;
