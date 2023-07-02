aabd027b8a317704928cd61a9eb8f77601b58237
unknown <gunaorusty@gmail.com>
2023-07-02 11:59:02 +0800

    fix scroolling issue

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Navbar effect while scrolling
 window.addEventListener('scroll', function() {
   var navbar = document.querySelector('.navbar');
   var scrollPosition = window.scrollY;
@@ -9,6 +10,7 @@ window.addEventListener('scroll', function() {
   }
 });
 
+// Smooth scroll animation with overshoot effect
 function smoothScroll(target, duration) {
   const targetElement = document.querySelector(target);
   const navbarHeight = document.querySelector('.navbar').offsetHeight;
@@ -16,20 +18,22 @@ function smoothScroll(target, duration) {
   const startPosition = window.scrollY;
   const distance = targetPosition - startPosition;
   let startTime = null;
+  const container = document.documentElement;
 
   function animation(currentTime) {
     if (startTime === null) startTime = currentTime;
     const timeElapsed = currentTime - startTime;
-    const ease = easeInOutCubic(timeElapsed, startPosition, distance, duration);
-    window.scrollTo(0, ease);
-    if (timeElapsed < duration) requestAnimationFrame(animation);
-  }
 
-  function easeInOutCubic(t, b, c, d) {
-    t /= d / 2;
-    if (t < 1) return (c / 2) * t * t * t + b;
-    t -= 2;
-    return (c / 2) * (t * t * t + 2) + b;
+    // Modify the easing function here for the desired effect
+    if (timeElapsed < duration) {
+      const progress = timeElapsed / duration;
+      const overshoot = Math.sin(progress * (Math.PI / 2)); // Adjust overshoot strength
+      const ease = startPosition + distance * overshoot;
+      container.scrollTo(0, ease);
+      requestAnimationFrame(animation);
+    } else {
+      container.scrollTo(0, targetPosition);
+    }
   }
 
   requestAnimationFrame(animation);
@@ -55,8 +59,15 @@ document.querySelectorAll(['.navbar-nav .nav-link', '.contact-btn .nav-link']).f
   });
 });
 
+// textare auto resize height
+function adjustTextareaHeight() {
+  const textarea = document.getElementById('text-area');
+  textarea.style.height = 'auto'; // Reset the height to auto to recalculate
+
+  // Set the new height to fit the content
+  textarea.style.height = textarea.scrollHeight + 'px';
+}
+
 // Get the current year
-
 const currentYear = new Date().getFullYear();
-
   document.getElementById("current-year").textContent = currentYear;
